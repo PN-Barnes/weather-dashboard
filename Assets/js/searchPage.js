e02ff9df5fd96c0135ac102f818948049e980ff1
PN-Barnes <paulieb99@gmail.com>
2021-07-04 18:58:41 -0700

    access data within the getapi function to start displaying to the page

--- Assets/js/searchPage.js
+++ Assets/js/searchPage.js
@@ -3,18 +3,37 @@ var todayInfo  = document.getElementById('todayInfo')
 var fiveForecastContainer = document.getElementById('fiveContent')
 var inputKey = document.getElementById('inputKey');
 var apiKey = 'dae35eafecabbca38e28c2fc1f8371c6'
+var searchCity = document.getElementById('city')
+var currentDay = moment().format('MMMM Do YYYY');
+var todayDate = document.getElementById('Date')
+var todayTemp = document.getElementById('todayTemp')
+var todayHumidity = document.getElementById('todayHumidity')
+var todayWind = document.getElementById('todayWind')
+var todayUV = document.getElementById('todayUV')
+var todayIcon = document.getElementById('icon')
+console.log(currentDay);
+
 
 function getApi() {
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputKey.value}&appid=dae35eafecabbca38e28c2fc1f8371c6`)
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${inputKey.value}&appid=dae35eafecabbca38e28c2fc1f8371c6&units=imperial`)
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
     console.log(`${inputKey.value} raw data: `);
     //We need to examine the API response to know how to access the data we want to extract.
-    console.log(data);
-  });
+
+    searchCity.textContent = inputKey.value;
+    todayDate.textContent = currentDay;
+    todayTemp.textContent = 'Temperature: ' + data.main.temp;
+    todayHumidity.textContent = 'Humidity: ' + data.main.humidity +'%';
+    todayWind.textContent = 'Wind speed: ' + data.wind.speed;
+    todayIcon = data.weather[0].icon;
+    console.log(todayIcon)
+  })
 }
 
 searchButton.addEventListener('click', getApi);
+
+
